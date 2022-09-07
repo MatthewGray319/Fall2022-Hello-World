11796c041889219b61ae5a022111736955e7e205
Matthew Gray <matthewcharlesgray319@gmail.com>
2022-09-07 17:46:12 -0400

    Added code from Davis's link

--- Fall2022 Hello World/Fall2022 Hello World/Program.cs
+++ Fall2022 Hello World/Fall2022 Hello World/Program.cs
@@ -21,6 +21,9 @@ Console.WriteLine(Age);
 //Performs some kind of basic operation / action on some values
 //ex 5 + 13 = 18
 
+
+//Assignment operator
+
 //Arithmetic
 // Addition, subtraction, multiplication, division, exponentiation (^), modulo (%) returns the remainder after dividing by a particular number
 var StudentCount = 5 + 13;
@@ -42,3 +45,15 @@ Console.WriteLine(time);
 //Parsing
 var ParsedValue = Int32.Parse("15"); //Integer 15
 Console.WriteLine(ParsedValue+5);
+
+// Pasted from Davis's link
+int a, b, c;
+a = 7;
+b = 7;
+c = b++;
+var message = $"a = {a}";
+var message2 = $"b = {b}";
+var message3 = $"c = {c}";
+Console.WriteLine(message);
+Console.WriteLine(message2);
+Console.WriteLine(message3);
